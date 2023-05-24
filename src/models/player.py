45d352c2de1b8319a179e40a6f0dc288fcf58b51
EL-WALID EL-KHABOU <alkhabou@gmail.com>
2023-05-24 12:48:53 +0200

    Création de ma premiere class dans /models/players.py

---
+++ src/models/player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, last_name, first_name, birthdate, gender, rank=0, tournament_points=0):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.birthdate = birthdate
+        self.gender = gender
+        self.rank = rank
+        self.tournament_points = tournament_points
+        
